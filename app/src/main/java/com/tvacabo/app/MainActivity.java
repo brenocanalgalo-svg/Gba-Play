a348d33e8d8727f30e7646a1788d78d66e39a9aa
brenocanalgalo-svg <brenocanalgalo@gmail.com>
2026-09-07 12:10:43 -0300

    Update MainActivity.java

--- app/src/main/java/com/tvacabo/app/MainActivity.java
+++ app/src/main/java/com/tvacabo/app/MainActivity.java
@@ -3,22 +3,33 @@ package com.tvacabo.app;
 import android.app.Activity;
 import android.os.Bundle;
 import org.mozilla.geckoview.GeckoRuntime;
-import org.mozilla.geckoview.GeckoRuntimeSettings;
 import org.mozilla.geckoview.GeckoSession;
 import org.mozilla.geckoview.GeckoView;
 
 public class MainActivity extends Activity {
+    private GeckoSession session;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         GeckoView view = findViewById(R.id.geckoview);
-        GeckoSession session = new GeckoSession();
+        session = new GeckoSession();
         
         GeckoRuntime runtime = GeckoRuntime.create(this);
         session.open(runtime);
         view.setSession(session);
         session.loadUri("https://harmonious-parfait-a4975b.netlify.app/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (session != null) {
+            // Volta para a página/canal anterior se houver histórico
+            session.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
